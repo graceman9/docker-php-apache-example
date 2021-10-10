b7e0bfc833061cd297f2ce8cd6c7d284403b09be
Sergey Klochko <graceman9@gmail.com>
2021-10-11 00:43:17 +0300

    Added php work with mysql example

--- php/src/index.php
+++ php/src/index.php
@@ -1,3 +1,33 @@
 <?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-echo 123;
+// The MySQL service named in the docker-compose.yml.
+$host = 'db';
+
+// Database use name
+$user = 'MYSQL_USER';
+
+//database user password
+$pass = 'MYSQL_PASSWORD';
+
+$db = 'MYSQL_DATABASE';
+
+// check the MySQL connection status
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
+
+$result = $conn->query("SELECT * FROM test");
+if ($result) {
+    $users = $result->fetch_all(MYSQLI_ASSOC);
+    echo '<ul>Users:';
+    foreach ($users as $user) {
+        echo '<div>';
+        echo $user['id'], ': ', $user['name'];
+        echo '</div>';
+    }
+    echo '</ul>';
+}
